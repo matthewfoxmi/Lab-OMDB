dc3a631c871326b9401643cade277e466449e703
matthewfoxmi <matthewfoxmi@gmail.com>
2022-07-26 18:08:18 -0400

    Finished

---
+++ Lab OMDB/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Lab_OMDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab_OMDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        MovieDAL movieDAL = new MovieDAL();
+
+        List<MovieModel> movieList = new List<MovieModel>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet] //run by default, will just bring you to page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //run when searching
+        [HttpPost] //recevies search, will call DAL, get search term, grab result, display View
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet] //run when first get to page
+        public IActionResult MovieSearch() //overloading: 1 method has parameters, 1 does not
+        {
+            return View();
+        }
+
+        [HttpPost] //run after submitting form
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel result = movieDAL.GetMovie(title);
+            return View(result);
+        }
+
+        [HttpGet] //run when first get to page
+        public IActionResult MovieNight() //overloading: 1 method has parameters, 1 does not
+        {
+            return View();
+        }
+
+        [HttpPost] //run after submitting form
+        public IActionResult MovieNight(string title1, string title2, string title3)
+        {
+            List<MovieModel> movies = new List<MovieModel>()
+            //adds each title to list
+            {
+                 movieDAL.GetMovie(title1),
+                 movieDAL.GetMovie(title2),
+                 movieDAL.GetMovie(title3)
+            };
+
+            return View(movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab OMDB/Models/MovieDAL.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Lab_OMDB.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string title)
+        {
+            string url = $"http://www.omdbapi.com/?apikey=6047e6f7&t={title}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+
+            return result;
+        }        
+    }
+}
